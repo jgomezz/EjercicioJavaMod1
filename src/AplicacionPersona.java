1ad1f48bc02c92fa81eee16de92ed99c4d40f4c9
jgomezz <jgomezz@gmail.com>
2025-03-02 10:53:52 -0500

    update

---
+++ src/AplicacionPersona.java
@@ -0,0 +1,13 @@
+public class AplicacionPersona {
+
+    public static void main(String[] args) {
+
+        Persona p1 =  new Persona();
+
+        p1.nombres = "Alberto";
+
+
+        System.out.println(p1.nombres);
+    }
+
+}
---
+++ src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+
+    String nombres;
+    String apellido_materno;
+    String apellido_paterno;
+    String dni;
+
+    int edad;
+    char sexo;
+
+}
