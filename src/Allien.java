6af027e1671ca3e3a061d2a443110beba0d6097f
jgomezz <jgomezz@gmail.com>
2025-03-02 10:31:06 -0500

    update

---
+++ src/Allien.java
@@ -0,0 +1,9 @@
+public class Allien {
+
+    public String color;
+
+    public void accionAtacar(){
+        System.out.println("Ataca a colonizadores humanos");
+    }
+
+}
---
+++ src/Aplicacion.java
@@ -0,0 +1,13 @@
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        // objeto : primeraPeliculz
+        Allien primeraPeliculz = new Allien();
+
+        // objeto : segundaPeliculz
+        Allien segundaPeliculz = new Allien();
+
+
+    }
+}
