c9f819b6d08e2809a8e5c00b7b51b653ade82efa
jgomezz <jgomezz@gmail.com>
2025-03-09 10:53:13 -0500

    update

--- src/pe/edu/tecsup/_01/paquetes/ejercicio2/core/Aplicacion.java
+++ src/pe/edu/tecsup/_01/paquetes/ejercicio2/core/Aplicacion.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup._01.paquetes.ejercicio2.core;
 
 public class Aplicacion {
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/pe/edu/tecsup/_01/paquetes/ejercicio2/equipo/EquipoMovil.java
+++ src/pe/edu/tecsup/_01/paquetes/ejercicio2/equipo/EquipoMovil.java
@@ -1,4 +1,15 @@
 package pe.edu.tecsup._01.paquetes.ejercicio2.equipo;
 
 public class EquipoMovil {
+
+    protected String marca;
+    protected String modelo;
+    protected int anho;
+
+    public String obtenerInfo(){
+
+        return " Sin implementar";
+    }
+
+
 }
