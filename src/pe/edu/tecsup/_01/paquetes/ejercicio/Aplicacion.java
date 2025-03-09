e7a5db23cf07ba6f7ce2b034124c1b64e3f01172
jgomezz <jgomezz@gmail.com>
2025-03-09 10:26:27 -0500

    update

---
+++ src/pe/edu/tecsup/_01/paquetes/ejercicio/Aplicacion.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup._01.paquetes.ejercicio;
+
+import pe.edu.tecsup._01.paquetes.ejercicio.controlador.ProcesoControlador;
+import pe.edu.tecsup._01.paquetes.ejercicio.modelo.Proceso;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+
+        Proceso p =  new Proceso();
+        ProcesoControlador pc =  new ProcesoControlador();
+
+    }
+}
---
+++ src/pe/edu/tecsup/_01/paquetes/ejercicio/modelo/Proceso.java
@@ -0,0 +1,6 @@
+package pe.edu.tecsup._01.paquetes.ejercicio.modelo;
+
+public class Proceso {
+    String codigo;
+    String estado;
+}
