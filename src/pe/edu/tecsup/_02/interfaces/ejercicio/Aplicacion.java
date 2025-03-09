6e3f7c841dc4f2590ba3b7f26845d04bf1704bfb
jgomezz <jgomezz@gmail.com>
2025-03-09 12:24:42 -0500

    update

---
+++ src/pe/edu/tecsup/_02/interfaces/ejercicio/Aplicacion.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup._02.interfaces.ejercicio;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        Computadora c = new Computadora();
+
+        c.realizarJuegosEnLinea();
+;
+    }
+}
---
+++ src/pe/edu/tecsup/_02/interfaces/ejercicio/Computadora.java
@@ -0,0 +1,12 @@
+package pe.edu.tecsup._02.interfaces.ejercicio;
+
+public class Computadora implements SoporteTarjetaGrafica {
+
+    protected String tipoCpu;
+    protected String fabricante;
+
+    @Override
+    public void realizarJuegosEnLinea() {
+        System.out.println("Falta verificar si la computadora soporta juegos en linea");
+    }
+}
