13165f6a828420c07e017d4d162f48dfe5e8f6a8
jgomezz <jgomezz@gmail.com>
2025-03-09 08:18:28 -0500

    update

---
+++ src/_08/constante/Aplicacion.java
@@ -0,0 +1,8 @@
+package _08.constante;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/_08/constante/Bienvenida.java
@@ -0,0 +1,9 @@
+package _08.constante;
+
+public class Bienvenida {
+
+    public static void saludar(String idioma){
+
+    }
+
+}
