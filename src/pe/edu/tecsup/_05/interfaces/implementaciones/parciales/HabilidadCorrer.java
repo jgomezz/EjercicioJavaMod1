c06c93d13ccb3793c62965e0f9acf17dd224d633
jgomezz <jgomezz@gmail.com>
2025-03-09 13:00:42 -0500

    update

---
+++ src/pe/edu/tecsup/_05/interfaces/implementaciones/parciales/HabilidadCorrer.java
@@ -0,0 +1,5 @@
+package pe.edu.tecsup._05.interfaces.implementaciones.parciales;
+
+public interface HabilidadCorrer {
+    void correr();
+}
---
+++ src/pe/edu/tecsup/_05/interfaces/implementaciones/parciales/Persona.java
@@ -0,0 +1,9 @@
+package pe.edu.tecsup._05.interfaces.implementaciones.parciales;
+
+public class Persona extends PersonaBase {
+    @Override
+    public void correr() {
+        System.out.println("Tengo habilidad para CORRER");
+
+    }
+}
