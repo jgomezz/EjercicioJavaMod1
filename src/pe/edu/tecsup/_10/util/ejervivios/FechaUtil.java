556c2ca48a69198396b91160403955ed2f844ddf
jgomezz <jgomezz@gmail.com>
2025-03-16 09:25:52 -0500

    update

---
+++ src/pe/edu/tecsup/_10/util/ejervivios/FechaUtil.java
@@ -0,0 +1,20 @@
+package pe.edu.tecsup._10.util.ejervivios;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class FechaUtil {
+
+    final static String FORMAT_DATE = "dd/MM/YYYY";
+
+    public static String obtenerFechaActual() {
+
+        Calendar calendar = GregorianCalendar.getInstance();
+
+        SimpleDateFormat sdf = new SimpleDateFormat(FORMAT_DATE);
+
+        return sdf.format(calendar.getTime());
+
+    }
+}
